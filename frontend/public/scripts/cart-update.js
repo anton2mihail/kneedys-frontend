af6ebef44a6f946b9f66b18a990b46abf29e5e5a
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-11-17 18:15:59 -0500

    Copied the existing frontend

---
+++ frontend/public/scripts/cart-update.js
@@ -0,0 +1,143 @@
+// Gets info about products in the cart
+const getProductsInfoInCart = () => {
+  $.ajax({
+    url: "/local",
+    success: cart,
+    dataType: "json"
+  });
+};
+
+// Builds the page elements for the cart and sets an event listener for the removal of the cart items
+const cart = (data) => {
+  buildProduct(data)
+    .then(renderEls)
+    .then(() => {
+      $(".product-remove").on("click", function () {
+        let tr = $(this).closest("tr");
+        let c = tr.children(".product-name");
+        let p = tr.children(".price");
+        let $tax = $(".tax");
+        let $total = $(".total");
+        let val = c
+          .children(".name")
+          .text()
+          .trim();
+        let price = p
+          .text()
+          .trim();
+        let subtotal = (($(".subtotal").text().substr(1)) - (price.substr(1))).toFixed(2);
+
+        let count = $(".counter").text();
+        count = parseInt(count) - 1;
+        $(".counter").text(count);
+        $(".subtotal").text(`$${subtotal}`);
+        $tax.text('$' + (subtotal * 0.13).toFixed(2));
+        $total.text('$' + (subtotal * 1.13).toFixed(2));
+        $(tr).remove();
+        $.ajax({
+          method: "POST",
+          url: "/local/delete",
+          data: {
+            search: val
+          },
+          success: () => {
+            console.log("It worked!");
+          }
+        });
+      });
+    });
+}
+
+// Sets quantity of the products and sends both that and their ids 
+const actualCart = id => {
+  let url = " https://kneedys-api.herokuapp.com/products/" + id;
+  $.get(url).then(response => {
+    response[0].quantity = 1;
+    $.ajax({
+      type: "POST",
+      url: "/cart",
+      data: {
+        response
+      },
+      success: res => {
+        console.log(res);
+      }
+    });
+  });
+  return Promise.resolve("");
+};
+
+
+// Onclick for the add to cart button
+$(".add-cart").on("click", function () {
+  let count = $(".counter").text();
+  count = parseInt(count) + 1;
+  $(".counter").text(count);
+  let div = $(this).closest("div");
+  let productId = div.find(".productID").text();
+  actualCart(productId);
+});
+
+// Builds the html elements for the cart items
+const buildProduct = (info) => {
+  let objs = [];
+  let $subtotal = $(".subtotal");
+  let $tax = $(".tax");
+  let $total = $(".total");
+  let subtotal = 0;
+  info.forEach(element => {
+    let priceTrim = element.quantity;
+    let el = $(`
+        <tr class="text-center">
+		    <td class="product-remove"><button class="btn btn-outline-black"><span class="icon-close"></span></button></td>
+		    <td class="image-prod">
+		        <div class="img" style='background-image:url("${element.imgUrl}");'></div>
+            </td>
+    	    <td class="product-name">
+		        <h3 class="name">${element.name}</h3>
+		        <p class="description">${element.description}</p>
+	        </td>
+		    <td class="price">$${element.price}</td>
+		    <td class="quantity">
+		        <div class="input-group mb-3">
+		            <input type="text" name="quantity" class="quantity form-control input-number" value="${element.quantity}" min="1" max="100">
+		        </div>
+		    </td>
+		    <td class="item-total">$${(element.price * priceTrim).toFixed(2)}</td>
+		</tr>`);
+    objs.push(el);
+    subtotal += element.price * priceTrim;
+    $subtotal.text('$' + subtotal.toFixed(2));
+    $tax.text('$' + (subtotal * 0.13).toFixed(2));
+    $total.text('$' + (subtotal * 1.13).toFixed(2));
+  });
+
+  return Promise.resolve(objs);
+}
+
+// Renders the cart items
+const renderEls = (els) => {
+  els.forEach(el => {
+    $("tbody").append(el);
+  });
+}
+
+$(".clear-cart").on("click", function () {
+  $(".root").empty();
+});
+
+// Only gets info if this is the cart page
+if (window.cart) {
+  console.log("Cart Page!");
+  getProductsInfoInCart();
+}
+
+const cartPrice = () => {
+  let $subtotal = $(".subtotal2");
+  let $tax = $(".tax2");
+  let $total = $(".total2");
+
+  $subtotal.text('$' + globalSubtotal.toFixed(2));
+  $tax.text('$' + (subtotal * 0.13).toFixed(2));
+  $total.text('$' + (subtotal * 1.13).toFixed(2));
+}
---
+++ frontend/server.js
@@ -0,0 +1,236 @@
+"use strict";
+
+require('dotenv').config();
+const PORT = process.env.PORT || 4040;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const sass = require("node-sass-middleware");
+const fetch = require('node-fetch');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+const userDB = require('./db');
+
+// Session based management
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+// For session cookies
+app.use(require('cookie-parser')());
+
+// File system db
+var loki = require('lokijs'),
+  db = new loki('quickstart.db');
+
+// Load a loki collection and return it
+function loadCollection(colName, callback) {
+  db.loadDatabase({}, function () {
+    var _collection = db.getCollection(colName);
+    if (!_collection) {
+      console.log("Collection %s does not exist. Creating ...", colName);
+      _collection = db.addCollection('sessionCart');
+    }
+    callback(_collection);
+  });
+}
+
+
+passport.use(new Strategy(
+  function (username, password, cb) {
+    userDB.users.findByUsername(username, function (err, user) {
+      if (err) {
+        return cb(err);
+      }
+      if (!user) {
+        return cb(null, false);
+      }
+      if (user.password != password) {
+        return cb(null, false);
+      }
+      return cb(null, user);
+    });
+  }));
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+  userDB.users.findById(id, function (err, user) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+
+app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use("/styles", sass({
+  src: __dirname + "/styles",
+  dest: __dirname + "/public/styles",
+  debug: false,
+  outputStyle: 'expanded'
+}));
+app.use(express.static("public"));
+
+// Home page
+app.get('/', (req, res) => {
+  res.render('index', {
+    user: req.user
+  });
+});
+
+app.get('/login', (req, res) => {
+  res.render('login', {
+    user: req.user
+  });
+});
+
+app.post('/login',
+  passport.authenticate('local', {
+    failureRedirect: '/login'
+  }), (req, res) => {
+    res.redirect('/');
+  });
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+app.get("/about", (req, res) => {
+
+  res.render("about", {
+    user: req.user
+  });
+});
+app.get("/cart", (req, res) => {
+  res.render("cart", {
+    "cart": true,
+    user: req.user
+  });
+});
+
+app.get('/local', (req, res) => {
+  loadCollection('sessionCart', function (col) {
+    //show the users
+    res.json(col.data);
+    db.saveDatabase();
+  });
+})
+app.post('/local/delete', (req, res) => {
+  loadCollection('sessionCart', function (col) {
+    let search = req.body.search;
+    console.log(search);
+    //show the users
+    col.findAndRemove({
+      'name': {
+        '$eq': search
+      }
+    });
+    db.saveDatabase();
+  });
+  res.end();
+})
+
+app.get("/checkout", (req, res) => {
+  res.render("checkout", {
+    user: req.user,
+    checkout: true
+  });
+});
+
+app.post('/checkout', (req, res) => {
+  loadCollection('sessionCart', function (col) {
+    //show the users
+    let data = col.data;
+    let order = data.reduce((acc, el) => {
+      acc += 'id: ' +
+        el.id + ', quantity:' + el.quantity + ' ';
+      return acc;
+    }, "");
+    const url = ' https://kneedys-api.herokuapp.com/text';
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify([{
+        "number": "2267007741",
+        "message": "Youve placed an order, please wait, the restaurant will send you the eta."
+      }, {
+        "number": "4165222220",
+        "message": req.body.fname + ', ' + req.body.lname + " Has placed order, " + order + ", Send eta:"
+      }])
+    }).then((response) => {
+      col.chain().find({}).remove()
+      db.saveDatabase(function (err) {
+        res.redirect('/order-received');
+      });
+    }).catch(err => {
+      console.log(err);
+    });
+  });
+
+});
+
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    user: req.user
+  });
+});
+app.get("/order", (req, res) => {
+  const url = ' https://kneedys-api.herokuapp.com/products';
+  fetch(url, {
+    method: 'GET',
+  }).then((response) => {
+    response.body.on('data', (e) => {
+      let productDatabase = JSON.parse(e);
+      let templateVars = {
+        product: productDatabase,
+        user: req.user
+      };
+      res.render("order", templateVars);
+    });
+  }).catch(err => {
+    console.log(err);
+  });
+});
+
+app.get("/order-received", (req, res) => {
+  res.render("order-received", {
+    user: req.user,
+  });
+});
+
+
+
+app.post('/cart', (req, res) => {
+  let items = req.body.response[0];
+  loadCollection('sessionCart', function (col) {
+    let found = false;
+    col.findAndUpdate({
+      'name': {
+        '$eq': items.name
+      }
+    }, (el) => {
+      found = true;
+      el = el.quantity++;
+    });
+    if (found === false) {
+      col.insert(items);
+    }
+    //save 
+    db.saveDatabase();
+  });
+  res.end();
+});
+
+app.listen(PORT, () => {
+  console.log("Front End Server listening on port " + PORT);
+});
